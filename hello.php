ecc8efa4fcc9df141b0c67876fb2cdc65bfdb260
Shitanshu <kkray459@gmail.com>
2026-03-30 20:52:35 +0530

    Task 1 hello.php added

---
+++ hello.php
@@ -0,0 +1,9 @@
+<?php
+  // Ye ek simple heading dikhayega
+  echo "<h1>Success! My PHP Environment is Ready.</h1>";
+  
+  // Ye line aapke server ka current time aur date dikhayegi
+  echo "<p>Current Server Time: " . date("h:i:sa") . "</p>";
+
+  echo "<p>Main ab Apex Planet ke roadmap ka Task-1 .</p>";
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<h1>Hello World!</h1>";
+    echo "<h3>My PHP Environment is working perfectly.</h3>";
+    echo "Current PHP Version: " . phpversion();
+?>
